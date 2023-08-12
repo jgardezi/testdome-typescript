8009e9df368f35a573721680fa621a9951c26a13
Javed Gardezi <javed.gardezi@primexconnect.com>
2023-08-12 14:17:10 +1000

    Removed nodemon for now use vscode for debugging as it provides much better and simpler flow

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 TODO: setup and project structure documentation
+
+From command line issue the following command
+
+```bash
+npm start
+```
--- src/Positionable.ts
+++ src/Positionable.ts
@@ -1,3 +1,18 @@
 class Positionable {
-  
+  locationX: number = 0;
+  locationY: number = 0;
+
+  moveTo(toX: number, toY: number) {
+    this.locationX = toX;
+    this.locationY = toY;
+  }
 }
+
+class Rotatble {
+  orientation: number = 0;
+
+  rotate(orientation: number) {
+    this.orientation += orientation;
+  }
+}
+
